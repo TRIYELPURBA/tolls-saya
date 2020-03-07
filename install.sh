3d26ea39f9e71fb36e76d8d1c86722155ef86ce3
CYBER ROOT <61105088+TRIYELPURBA@users.noreply.github.com>
2020-03-07 06:48:11 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,2 @@
-pkg install python
-pkg install python2
-pip2 install mechanize
-pkg install ruby -y2
-apt install nano
-pkg install Mtools
-pkg install openssh
-pip2 install requests
-pip2 install mechanize
-pip2 install -r requirements.txt
-pip2 install Bs4
-pip2 install urllib3 chardet certifi idna requests
-pkg install git
-pkg install curl
-pkg install wget
-pkg install php
+pkg install ruby 
+gem install lolcat 
