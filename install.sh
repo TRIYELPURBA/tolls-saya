983dcbbe6c9a69f7a25f77ce1b353bd3eb770f80
TRIYELPURBA <61105088+TRIYELPURBA@users.noreply.github.com>
2020-03-07 12:08:37 +0700

    sasas
    sasas

---
+++ install.sh
@@ -0,0 +1,16 @@
+pkg install python
+pkg install python2
+pip2 install mechanize
+pkg install ruby -y2
+apt install nano
+pkg install Mtools
+pkg install openssh
+pip2 install requests
+pip2 install mechanize
+pip2 install -r requirements.txt
+pip2 install Bs4
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pkg install curl
+pkg install wget
+pkg install php
